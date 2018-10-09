d24af875e2721b0289d2ebc417ba9aee2cc57714
waterthreshold <43978378+waterthreshold@users.noreply.github.com>
2018-10-09 11:25:57 +0800

    Add files via upload

---
+++ joanna_odd.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+int main ()
+{
+	
+	unsigned long long int n;
+	while (scanf ("%d",&n) && n)
+	{
+		unsigned long long int num;
+		int i;
+		unsigned long long int sum=0;
+		num=((1+n)*((n-1)/2+1))/2; 
+		for (i =0;i<3;i++)
+			sum += (1+2*(num-1-i));
+			printf ("%llu\n",sum);
+	}
+	
+	return 0;
+}
