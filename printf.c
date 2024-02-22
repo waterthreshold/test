b2bd6ffb942dbf8256e2c0fdfec4c4f7b58f8e52
Your <you@example.com>
2024-02-22 17:48:00 +0800

    get more fancy and progress status percentage

--- printf.c
+++ printf.c
@@ -4,10 +4,12 @@
 int main (){
 	int i =0 ;
 	while (i++ < 100 ){
-		if (i%2!=0)
-			printf("/\b");
-		else 
-			printf("\\\b");
+		if (i%3==1)
+			printf("%d%%... /\r",i);
+		else if (i%3==2)
+			printf("%d%%... -\r",i);
+		else
+			printf("%d%%... \\\r",i);
 		fflush (stdout);
 		usleep(100000);
 	}
